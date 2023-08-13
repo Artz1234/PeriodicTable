1033ea6a95e73ce51944e246d21849723a8fd02f
Artz1234 <139537046+Artz1234@users.noreply.github.com>
2023-08-13 21:14:19 +0545

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Periodic Table
+        </title>
+        <link rel="icon" type="image/x-icon" href="icon.ico">
+        <link rel="stylesheet" href="common.css">
+        <script src="table.js"></script>
+    </head>
+    <body>
+        <?php
+        echo "Hello world"
+        ?>
+    </body>
+</html>
